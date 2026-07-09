b57cae49a71cf7e1bf460e7387b56e135b975c67
Alex Garcia Amezcua <alexgadev@hotmail.com>
2026-07-09 09:32:22 +0200

    4-first version passes all tests, runtime 0 ms

--- 4-Median_of_Two_Sorted_Arrays.cpp
+++ 4-Median_of_Two_Sorted_Arrays.cpp
@@ -5,21 +5,55 @@ using namespace std;
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int size = nums1.size() + nums2.size();
-        if(size % 2 == 0){
-            if(size > nums1.size()){
+    void merge(vector<int>& arr, vector<int>& l, vector<int>& r){
+        int left = 0, right = arr.size() - 1;
 
-            }
-            else if(size == nums1.size()){
+        int mid = left + (right - left) / 2;
 
+        int i = 0, j = 0;
+        int k = left;
+
+        // Merge the vectors into arr[left..right]
+        while(i < l.size() && j < r.size()){
+            if(l[i] <= r[j]){
+                arr[k] = l[i];
+                i++;
             }
             else{
-
+                arr[k] = r[j];
+                j++;
             }
+            k++;
+        }
+
+        // copy the remaining elements of l[], if there are any
+        while(i < l.size()){
+            arr[k] = l[i];
+            i++;
+            k++;
+        }
+
+        // copy the remaining elements of r[], if there are any
+        while(j < r.size()){
+            arr[k] = r[j];
+            j++;
+            k++;
+        }
+    }
+
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> arr(nums1.size() + nums2.size());
+        merge(arr, nums1, nums2);
+
+
+        int size = arr.size();
+
+        int index = size / 2;
+        if(size % 2 == 0){
+            return static_cast<double>((arr[index - 1] + arr[index]) / 2.0);
         }
         else{
-            return (size / 2) >= nums1.size() ? static_cast<double>(nums2[(size / 2) % nums1.size()]) : static_cast<double>(nums1[size / 2]);
+            return static_cast<double>(arr[index]);
         }
     }
 };
@@ -27,8 +61,8 @@ public:
 int main(){
     Solution sol = Solution();
 
-    vector<int> nums1 = {1, 3};
-    vector<int> nums2 = {2};
+    vector<int> nums1 = {1, 2};
+    vector<int> nums2 = {3, 4};
 
     std::cout << sol.findMedianSortedArrays(nums1, nums2) << std::endl;
 }
